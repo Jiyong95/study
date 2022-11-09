e272bc63039d977803aed6e0e7a98676628fac5c
Leejiyong <ljy1110@midasin.com>
2022-11-09 18:28:00 +0900

    [threeJs] 배경색 다루기

--- src/threeJs/perspectiveCamera.tsx
+++ src/threeJs/perspectiveCamera.tsx
@@ -1,6 +1,9 @@
 'use client';
 import { useEffect, useRef } from 'react';
 import * as THREE from 'three';
+import classnames from 'classnames/bind';
+import styles from './threeJs.module.scss';
+const cx = classnames.bind(styles);
 
 /*PerspectiveCamera : 원근감이 있는 카메라*/
 const PerspectiveCamera = () => {
@@ -12,12 +15,16 @@ const PerspectiveCamera = () => {
     const renderer = new THREE.WebGLRenderer({
       canvas: canvasRef.current,
       antialias: true,
+      alpha: true, //배경색(true = scss의 색 적용)
     });
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1); //고해상도로 표현하기 위함. ex) 2배 크기의 이미지를 가져와 절반의 크기로 표현하는 것과 같음.
+    renderer.setClearColor(0x00ff00); //배경색 직접 설정
+    renderer.setClearAlpha(0.5); //opacity와 비슷한 효과(setClearColor /2 + backgroundColor 합친 색)
 
     /* Scene(공간)*/
     const scene = new THREE.Scene();
+    scene.background = new THREE.Color('blue'); //scene에 적용하면 renderer에 적용된 색상 무시됨.
 
     /*Camera(시점) https://threejs.org/docs/index.html#api/ko/cameras/PerspectiveCamera*/
     const camera = new THREE.PerspectiveCamera(
@@ -57,7 +64,11 @@ const PerspectiveCamera = () => {
       window.removeEventListener('resize', () => setSize(camera, window));
   }, []);
 
-  return <canvas ref={canvasRef} />;
+  return (
+    <div className={cx('wrapper')}>
+      <canvas ref={canvasRef} />
+    </div>
+  );
 };
 
 export default PerspectiveCamera;
